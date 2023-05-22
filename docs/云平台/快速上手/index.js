f3c405f43c709cc8d2e56b8c5bc638030db7e51a
Iric <iricbing@gmail.com>
2023-05-22 18:35:45 +0800

    增加部分设备管理文档

--- docs/云平台/快速上手/index.js
+++ docs/云平台/快速上手/index.js
@@ -1,5 +1,8 @@
 module.exports = {
   type: "category",
   label: "快速上手",
-  items: ["云平台/设备管理/设备接入/README"],
+  items: [
+    "云平台/设备管理/设备接入/README",
+    "云平台/摄像头管理/萤石摄像头接入/README",
+  ],
 };
--- docs/云平台/设备管理/index.js
+++ docs/云平台/设备管理/index.js
@@ -1,5 +1,11 @@
 module.exports = {
   type: "category",
   label: "设备管理",
-  items: ["云平台/设备管理/设备接入/README"],
+  items: [
+    "云平台/设备管理/设备接入/README",
+    "云平台/设备管理/参数配置/README",
+    "云平台/设备管理/云端调试/README",
+    "云平台/设备管理/升级固件/README",
+    "云平台/设备管理/绑定摄像头/README",
+  ],
 };
