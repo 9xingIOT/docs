a3c8db8fc658fd50d5bb068930bbc1011dc66565
Iric <iricbing@gmail.com>
2023-05-23 13:45:45 +0800

    增加萤石云摄像头接入文档

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
       }),
     ],
   ],
-  plugins: ["@docusaurus/theme-live-codeblock"],
+  plugins: ["@docusaurus/theme-live-codeblock", ["drawio", {}]],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
@@ -156,22 +156,22 @@ const config = {
         indexName: "prod_docs",
       },
       mermaid: {
-        theme: {light: 'forest', dark: 'forest'},
+        theme: { light: "forest", dark: "forest" },
       },
     }),
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
   markdown: {
     mermaid: true,
   },
-  themes: ['@docusaurus/theme-mermaid'],
+  themes: ["@docusaurus/theme-mermaid"],
 };
 
 module.exports = config;
